e4192d4d15f4698879b160be8e2279110428fec5
Firman Farel Richardo <farelpasaribu04@gmail.com>
2025-09-19 22:53:48 +0700

    update

--- database/config.php
+++ database/config.php
@@ -8,7 +8,6 @@ function connect_to_database() {
     $conn = new mysqli($host, $user, $pass, $db);
 
     if ($conn->connect_error) {
-        // Hentikan eksekusi dan log error tanpa menampilkan detail sensitif ke user
         error_log("Koneksi database gagal: " . $conn->connect_error);
         die("Koneksi ke database gagal. Silakan coba lagi nanti.");
     }
